44392f6a0f0f020abf1679c574fbb5f74639ee17
neo <emindemir1541@proton.me>
2024-12-10 13:38:39 +0300

    #135 -> #1, #2

---
+++ lib/presentation/common/screen/app_is_getting_ready_screen.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce_app_mobile/common/ui/theme/AppStyles.dart';
+import 'package:ecommerce_app_mobile/presentation/common/widgets/form_info_skeleton.dart';
+import 'package:ecommerce_app_mobile/sddklibrary/helper/ui_helper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../common/ui/assets/AppImages.dart';
+import '../../../common/ui/theme/AppSizes.dart';
+import '../../../common/ui/theme/AppText.dart';
+
+class AppIsGettingReadyScreen extends StatelessWidget {
+  const AppIsGettingReadyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(AppSizes.defaultPadding),
+        child: FormInfoSkeleton(
+          image: context.isDarkMode
+              ? AppImages.serverErrorDark
+              : AppImages.serverError,
+          message: AppText
+              .appIsGettingReadyPageAppIsGettingReady.capitalizeFirstWord.get,
+          notSvg: true,
+        ),
+      ),
+    );
+  }
+}
