a6f527e32e28255c9c9c38352af0bbf140606e5d
neo <emindemir1541@proton.me>
2024-12-12 16:36:24 +0300

    categories chip in bottom sheet product details

--- lib/presentation/common/widgets/chip_right_arrow.dart
+++ lib/presentation/common/widgets/chip_right_arrow.dart
@@ -3,12 +3,14 @@ import 'package:ecommerce_app_mobile/sddklibrary/ui/widget_clickable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../common/ui/theme/AppColors.dart';
+
 class ChipRightArrow extends StatelessWidget {
   const ChipRightArrow({super.key, required this.label, });
 
   final String label;
   @override
   Widget build(BuildContext context) {
-    return Chip(label: Text(label),shape: AppStyles.rightArrowOutlinedBorder,);
+    return Chip(label: Text(label),shape: AppStyles.rightArrowOutlinedBorder.copyWith(side: const BorderSide(color: Colors.black)),);
   }
 }
--- lib/presentation/search/widget/categories_lister_widget_sliver.dart
+++ lib/presentation/search/widget/categories_lister_widget_sliver.dart
@@ -1,20 +1,23 @@
+import 'package:ecommerce_app_mobile/common/ui/theme/color_filters.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../../../common/ui/assets/AppImages.dart';
+import '../../../common/ui/theme/AppColors.dart';
 import '../../../common/ui/theme/AppSizes.dart';
 import '../../../common/ui/theme/AppText.dart';
 import '../../../data/model/categories.dart';
 import '../../../data/model/category.dart';
 import '../../../data/model/category_struct.dart';
+import '../../../sddklibrary/ui/widget_clickable_outlined.dart';
+import '../../common/widgets/chip_right_arrow.dart';
 import '../../products/widget/product_list_tile.dart';
 
 class CategoriesListerWidgetSliver extends StatefulWidget {
   final Categories categories;
   final Function(Category category) onLastItemPressed;
-  const CategoriesListerWidgetSliver({
-    super.key,
-    required this.categories,
-    required this.onLastItemPressed
-  });
+
+  const CategoriesListerWidgetSliver({super.key, required this.categories, required this.onLastItemPressed});
 
   @override
   State<CategoriesListerWidgetSliver> createState() => _CategoriesListerWidgetSliverState();
@@ -31,46 +34,45 @@ class _CategoriesListerWidgetSliverState extends State<CategoriesListerWidgetSli
 
   @override
   Widget build(BuildContext context) {
-    // Return a list of slivers that can be used in a CustomScrollView
     return SliverList(
       delegate: SliverChildListDelegate([
-        // Header section
-        SizedBox(
-          height: 60,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: AppSizes.defaultPadding,
-                vertical: AppSizes.defaultPadding / 2
-            ),
-            child: Row(
-              children: [
-                currentCategoryStruct.isFirstLayer
-                    ? const Flexible(flex: 1, child: SizedBox())
-                    : Flexible(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            currentCategoryStruct.previousLayer();
-                          });
-                        },
-                        child: const Icon(Icons.arrow_back)
+        Padding(
+          padding: const EdgeInsets.all(AppSizes.defaultPadding / 2),
+          child: Row(
+            children: [
+              currentCategoryStruct.isFirstLayer
+                  ? const SizedBox.shrink()
+                  : Flexible(
+                      child: SizedBox(
+                        width: 35,
+                        height: 35,
+                        child: ClickableWidgetOutlined(
+                          onPressed: () {
+                            setState(() {
+                              currentCategoryStruct.previousLayer();
+                            });
+                          },
+                          child: SvgPicture.asset(
+                            AppImages.arrowLeft,
+                            height: 24,
+                            colorFilter: ColorFilters.oneColorForIcon(AppColors.blackColor),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Text(
-                  currentCategoryStruct.isFirstLayer
-                      ? AppText.commonPageCategories.capitalizeFirstWord.get
-                      : "${AppText.commonPageCategories} > ${currentCategoryStruct.title}",
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20
-                  ),
-                ),
-              ],
-            ),
+              Wrap(
+                children: [
+                  ...List.generate(currentCategoryStruct.categoryNode.length, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                        left: AppSizes.spaceBtwHorizontalFieldsSmall,
+                      ),
+                      child: ChipRightArrow(label: currentCategoryStruct.categoryNode.node[index].name),
+                    );
+                  }),
+                ],
+              )
+            ],
           ),
         ),
 
@@ -82,18 +84,15 @@ class _CategoriesListerWidgetSliverState extends State<CategoriesListerWidgetSli
         // List items
         ...List.generate(
           currentCategoryStruct.currentLayer.length,
-              (index) => ProductListTile(
+          (index) => ProductListTile(
             title: currentCategoryStruct.currentLayer[index].name,
-            isShowBottomBorder: currentCategoryStruct.currentLayer[index].id ==
-                currentCategoryStruct.currentLayer.last.id,
+            isShowBottomBorder: currentCategoryStruct.currentLayer[index].id == currentCategoryStruct.currentLayer.last.id,
             press: () {
-              if (currentCategoryStruct.isLastLayer(
-                  currentCategoryStruct.currentLayer[index])) {
+              if (currentCategoryStruct.isLastLayer(currentCategoryStruct.currentLayer[index])) {
                 widget.onLastItemPressed(currentCategoryStruct.currentLayer[index]);
               } else {
                 setState(() {
-                  currentCategoryStruct.nextLayer(
-                      currentCategoryStruct.currentLayer[index]);
+                  currentCategoryStruct.nextLayer(currentCategoryStruct.currentLayer[index]);
                 });
               }
             },
