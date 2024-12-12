5bfe03fe0adec9c02f593b44cbeeaf9b15041c54
neo <emindemir1541@proton.me>
2024-12-12 12:47:23 +0300

    #109 -> #1

--- lib/presentation/products/page/product_details_bottom_sheet.dart
+++ lib/presentation/products/page/product_details_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app_mobile/common/ui/theme/AppColors.dart';
+import 'package:ecommerce_app_mobile/common/ui/theme/AppStyles.dart';
 import 'package:ecommerce_app_mobile/common/ui/theme/AppText.dart';
 import 'package:ecommerce_app_mobile/presentation/common/widgets/app_bar_pop_back.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 
+import '../../../common/ui/theme/AppSizes.dart';
 import '../../../data/model/product_details_item.dart';
 
 class ProductDetailsBottomSheet extends StatelessWidget {
@@ -25,31 +27,38 @@ class ProductDetailsBottomSheet extends StatelessWidget {
             Expanded(
               child: ListView.builder(
                 itemCount: items.length,
-                itemBuilder: (context, index) => Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 48,
-                          decoration: BoxDecoration(border: Border.all(width: 0.7,color: AppColors.blackColor)),
-                          child: Center(
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 3, horizontal: AppSizes.defaultPadding / 2),
+                  child: Container(
+                    decoration: AppStyles.defaultBoxDecoration.copyWith(border: Border.all(color: AppColors.greyColor)),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                              child: Center(
                             child: Text(
                               items[index].title,
                               style: Theme.of(context).textTheme.titleMedium,
                             ),
                           )),
-                    ),
-                    Expanded(
-                      child: Container(
-                          height: 48,
-                          decoration: BoxDecoration(border: Border.all(width: 0.7,color: AppColors.blackColor)),
-                          child: Center(
-                            child: Text(
-                              items[index].content,
-                              style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 16),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                              child: Center(
+                            child: Flexible(
+                              child: Padding(
+                                padding: const EdgeInsets.all(6),
+                                child: Text(
+                                  items[index].content,
+                                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 16,color: Theme.of(context).primaryColor),
+                                ),
+                              ),
                             ),
                           )),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
